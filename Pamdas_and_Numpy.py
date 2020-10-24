b50670adeeed7102cf64944a42ad4863d37fcc2c
cljones97 <cljones4@millersville.edu>
2020-10-24 12:25:15 -0400

    Added some files from IBM Notebook and ran Pandas on them

--- Pamdas_and_Numpy.py
+++ Pamdas_and_Numpy.py
@@ -10,7 +10,7 @@ import numpy as np
 csv_path = file1.csv  #stores path of csv
 df = pandas.read_csv(csv_path) #result is stored in variable df which represents dataframe
 df.head() #examines first 5 rows of dataframe
-"""
+
 # Practice dataset
 data = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj', 'Chris', 'Jones', 'Windsor', 'Mike', 'Jet', 'Smith'],
         'Age':[27, 24, 22, 32, 22, 23, 19, 40, 53, 37],
@@ -37,16 +37,16 @@ print("##############################################")
 # You use column numbers and row numbers to get rows or columns at particular positions in the data frame.
 # iloc will return an IndexError if the requested indexer is out-of-bounds.
 
-print(df.iloc[0,0]) # row 0, col 0
-print(df.iloc[1,0]) # row 1, col 0
-print(df.iloc[0,1]) # row 0, col 1
-print(df.iloc[1,1]) # row 1, col 1
-print(df.iloc[2,0]) # row 2, col 0
-print(df.iloc[0,2]) # row 0, col 2
-print(df.iloc[2,1]) # row 2, col 1
-print(df.iloc[1,2]) # row 1, col 2
-print(df.iloc[2,2]) # row 2, col 2
-print(df.iloc[3,3]) # row 3, col 3
+print(df.iloc[0,0]) # row 1, col 1
+print(df.iloc[1,0]) # row 2, col 1
+print(df.iloc[0,1]) # row 1, col 2
+print(df.iloc[1,1]) # row 2, col 2
+print(df.iloc[2,0]) # row 3, col 0
+print(df.iloc[0,2]) # row 0, col 3
+print(df.iloc[2,1]) # row 3, col 2
+print(df.iloc[1,2]) # row 2, col 3
+print(df.iloc[2,2]) # row 3, col 3
+print(df.iloc[3,3]) # row 4, col 4
 print("##############################################")
 
 # By default, ix looks for a label. 
@@ -70,14 +70,53 @@ New_df = df[df['Age'] >= 30]
 #print(ageQual1) #Check back later
 print(New_df)
 #New_df.to_csv("Age_requirement.csv")
+"""
+# Practice from IBM Notebooks
+csv_path = 'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/Chapter%204/Datasets/TopSellingAlbums.csv'
+df = pd.read_csv(csv_path)
+print(df.head())
+print("##############################################")
 
+xlsx_path = 'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/Chapter%204/Datasets/TopSellingAlbums.xlsx'
+df2 = pd.read_excel(xlsx_path)
+print(df2.head())
+print("##############################################")
 
+# Access to the column Length
+x = df[['Length']]
+print(x)
+print("##############################################")
 
+# Get the column as a series
+y = df['Length']
+print(y)
+print("##############################################")
 
+# Get the column as a dataframe
+z = type(df[['Artist']])
+print(z)
+print("##############################################")
 
+# Access to multiple columns
+a = df[['Artist','Length','Genre']]
+print(a)
+print("##############################################")
 
+# Use a variable q to store the column Rating as a dataframe
+b = df[['Rating']]
+print(b)
+print("##############################################")
 
+# Assign the variable q to the dataframe that is made up of the column Released and Artist:
 
+c = df[['Released','Artist']]
+print(c)
+print("##############################################")
+
+# Access the 2nd row and the 3rd column of df:
+d = df.iloc[1,2]
+print(d)
+print("##############################################")
 
 
 
